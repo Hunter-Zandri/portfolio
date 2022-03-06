6ccb72b6c9c51dceb652cd4dcec99e2609465e71
Hunter-Zandri <zandrihunter@gmail.com>
2022-03-06 18:48:19 -0500

    trying to fix heroku deployment

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,17 @@
-
 import React from 'react';
-import photo from '/Users/hunterzandri/Desktop/portfolio/src/assets/cover/coverphoto.jpeg'
 
 function About() {
   return (
     <section className="my-5">
       <div className="my-2">
-        <div className="my-5">
-        <img className="profile-img" src={photo}></img>
-        {/* <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i> */}
+        <div className="profile-img my-5">
+        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
         </div>
         <p>
         My Name is Hunter Zandri, I'm Kennebunk Maine and im currently enrolled in UNH full stack coding bootcamp. 
-          I work at an engineering startup were we build gas delivery systems for the semiconductor Industry. 
-          I want to use what I what i learn in class in work and continue learning and practicing daily.</p>
+  I work at an engineering startup were we build gas delivery systems for the semiconductor Industry. 
+  I want to use what I what i learn in class in work and continue learning and practicing daily.
+        </p>
       </div>
     </section>
   );
