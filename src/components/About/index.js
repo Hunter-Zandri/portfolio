e17bb6fcb7e2139ab529ab6aa7d7baee99ea9fd5
Hunter-Zandri <zandrihunter@gmail.com>
2022-03-06 21:18:56 -0500

    took away profile picture to see if that fixes heroku diployment

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import coverImage from '/Users/hunterzandri/Desktop/portfolio/src/assets/cover/profile.jpg';
+
 function About() {
   return (
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-        <img src={coverImage} alt="wooden background"></img>
+        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
         </div>
         <p>
         My Name is Hunter Zandri, I'm Kennebunk Maine and im currently enrolled in UNH full stack coding bootcamp.
-        I work at an engineering startup were we build gas delivery systems for the semiconductor Industry. 
-        I want to use what I what i learn in class in work and continue learning and practicing daily.        </p>
+        I work at an engineering startup were we build gas delivery systems for the semiconductor Industry. I want to use what I what i learn in class in work and continue learning and practicing daily.        </p>
         <p>
         </p>
       </div>
